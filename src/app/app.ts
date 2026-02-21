90a02607ed656271db3ad5dba30eb380eac98507
Inés Carazo Núñez <ines.carazo355@comunidadunir.net>
2026-02-21 16:40:51 +0100

    Creación del repositorio
    (se me olvidó crear el repositorio y cuando me he dado cuenta ya tenía la práctica terminada)

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { BlogComponent } from "./components/blog/blog.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [BlogComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Actividad5_InesCarazoNunez');
+}
